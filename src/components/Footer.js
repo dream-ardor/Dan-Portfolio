2b08e0a5b42c6c088f02ba74296c06fcc5585f28
Dream-Ardor <barrick.dan80@gmail.com>
2018-10-19 21:49:36 -0700

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,10 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                        <li><a href="recruit_master" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+                        <li><a href="https://www.github.com/dream-ardor" className="icon fa-github"><span className="label">Github</span></a></li>
+                        <li><a href="danbarrick" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
+                        <li><a href="barrick.dan80@gmail.com" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
                     </ul>
                     <ul className="copyright">
                         <li>&copy; Dan Barrick</li><li>Design: <a href="https://www.dreamardor.com">Dan Barrick</a></li>
