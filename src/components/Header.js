01612f0a31bb22b2451c5a86b566a28ae8cb0f10
Dream-Ardor <barrick.dan80@gmail.com>
2018-10-19 20:36:35 -0700

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,9 +9,9 @@ class Header extends React.Component {
             <header id="header">
                 <div className="inner">
                     <a href="#" className="image avatar"><img src={avatar} alt="Dan Barrick" /></a>
-                    <h1><strong>I am Strata</strong>, a super simple<br />
-                    responsive site template freebie<br />
-                    crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+                    <h1><strong>I am Dan Barrick</strong>, a super motivated<br />
+                    and self-taught web developer with an entrepreneurial mindset.<br />
+                    Page Design by <a href="https:www.dreamardor.com">Dan Barrick</a>.</h1>
                 </div>
                 <Footer />
             </header>
