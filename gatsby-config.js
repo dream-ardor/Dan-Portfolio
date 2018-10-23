0f849167221f857c664b08f79157cf53f1e05f5c
Dan Barrick <barrick.dan80@gmail.com>
2018-10-22 17:49:18 -0700

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Strata by HTML5 UP",
-    author: "Hunter Chang",
+    title: "Gatsby Starter",
+    author: "Dan Barrick",
     description: "A Gatsby.js Starter based on Strata by HTML5 UP"
   },
   plugins: [
