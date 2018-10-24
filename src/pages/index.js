fd18183d564b3a7eafc70bd27f05b02dd9afea22
Dream-Ardor <barrick.dan80@gmail.com>
2018-10-23 18:53:58 -0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -120,9 +120,9 @@ class HomeIndex extends React.Component {
                             <div className="8u 12u$(small)">
                                 <form method="post" action="#">
                                     <div className="row uniform 50%">
-                                        <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
-                                        <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
-                                        <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
+                                        <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" label for="Name" /></div>
+                                        <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" label for="Email" /></div>
+                                        <div className="12u"><textarea name="message" id="message" label for="Message" rows="4"></textarea></div>
                                     </div>
                                 </form>
                                 <ul className="actions">
