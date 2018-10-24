3f4326b6e3c30d7056d9dee337303d177a5c7f2b
Dream-Ardor <barrick.dan80@gmail.com>
2018-10-23 18:46:41 -0700

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,10 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="https://www.linkedin.com/in/danbarrick" className="icon fa-linkedin"><span className="label">Twitter</span></a></li>
-                        <li><a href="https://www.github.com/dream-ardor" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="danbarrick" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                        <li><a href="barrick.dan80@gmail.com" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                        <li><a href="https://www.linkedin.com/in/danbarrick" aria-label="linkedin" className="icon fa-linkedin"><span className="label">Twitter</span></a></li>
+                        <li><a href="https://www.github.com/dream-ardor" aria-label="github" className="icon fa-github"><span className="label">Github</span></a></li>
+                        <li><a href="danbarrick" aria-label="dribble" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
+                        <li><a href="barrick.dan80@gmail.com" aria-label="email" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
                     </ul>
                     <ul className="copyright">
                         <li>&copy; Dan Barrick</li><li>Design: <a href="https://www.dreamardor.com">Dan Barrick</a></li>
